9b7db94a5c2e4fda4be218c3377a7afc190a2ebf
John Polling <john@theled.co.uk>
2017-04-25 11:36:16 +0100

    Feat (double lookup): Allow call to make a double call
    This basically grabs the first Postcode item and makes a second call
    with the container to get the Address items

--- lib/pca_predict/address_validation.ex
+++ lib/pca_predict/address_validation.ex
@@ -1,8 +1,23 @@
 defmodule PCAPredict.AddressValidation do
 
-  def lookup(text) do
+  def lookup(text, options \\ %{additional_lookup: false}) do
     PCAPredict.Client.request("/Capture/Interactive/Find/v1.00/json3ex.ws",
                           %{text: text})
+    |> process_response(options)
+  end
+
+  def lookup_container(container) do
+    PCAPredict.Client.request("/Capture/Interactive/Find/v1.00/json3ex.ws",
+                          %{container: container})
+  end
+
+  defp process_response({:error, errors}, _options), do:
+    {:error, errors}
+  defp process_response({:ok, items}, %{additional_lookup: false}), do:
+    {:ok, items}
+  defp process_response({:ok, items}, %{additional_lookup: true}) do
+    first_item = Enum.at(items, 0)
+    lookup_container(first_item.id)
   end
 
 end
--- test/support/capture_json_response.ex
+++ test/support/capture_json_response.ex
@@ -23,6 +23,25 @@ defmodule PCAPredict.Support.CaptureJsonResponse do
       """
   end
 
+  def find_response_with_address_objects do
+    """
+    {"Items":[{"Id":"GB|RM|A|26742664","Type":"Address",
+    "Text":"1 The Cottages, Moseley Road","Highlight":"",
+    "Description":"Hallow, Worcester, WR2 6NJ"},
+    {"Id":"GB|RM|A|26742665","Type":"Address","Text":"1 Wylcotts, Moseley Road",
+    "Highlight":"","Description":"Hallow, Worcester, WR2 6NJ"},
+    {"Id":"GB|RM|A|26742666","Type":"Address",
+    "Text":"2 The Cottages, Moseley Road","Highlight":"",
+    "Description":"Hallow, Worcester, WR2 6NJ"},
+    {"Id":"GB|RM|A|52269655","Type":"Address",
+      "Text":"2 Wylcotts, Moseley Road","Highlight":"",
+      "Description":"Hallow, Worcester, WR2 6NJ"},
+    {"Id":"GB|RM|A|26742627","Type":"Address",
+      "Text":"Beaumont, Moseley Road","Highlight":"",
+      "Description":"Hallow, Worcester, WR2 6NJ"}]}
+    """
+  end
+
   def invalid_key_response do
     """
     {
